ed6472a90a52effe918f29ae42b108143b091e68
adas3000 <youngtoss38@gmail.com>
2019-08-26 15:56:41 +0200

    Class appdata created

---
+++ app/src/main/java/com/mypackage/cafmp/Data/AppData.java
@@ -0,0 +1,21 @@
+package com.mypackage.cafmp.Data;
+
+import android.app.Application;
+import android.content.Context;
+
+public class AppData extends Application {
+
+    private static Context context;
+
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        context = this;
+    }
+
+
+    public static Context getContext(){return context;}
+
+
+}
